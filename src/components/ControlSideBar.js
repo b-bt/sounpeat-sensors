24677e99374076c56e58399d2bc935249dffc21e
Bruno Barbosa <bbt@cin.ufpe.br>
2018-12-02 16:15:13 -0300

    ControlSideBar with all the elements

--- src/components/ControlSideBar.js
+++ src/components/ControlSideBar.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
+import { Card, Button } from 'reactstrap';
 
-import { Card } from 'reactstrap';
+import RoundIndicator from './sidebar/RoundIndicator';
+import PlayerIndicator from './sidebar/PlayerIndicator';
+import NewGameModal from './sidebar/NewGameModal';
 
 class ControlSideBar extends Component {
   render() {
+    const { gameState } = this.props;
+    const { totalPlayers, currentPlayer } = gameState;
+
     const { containerStyle } = styles;
 
     return (
       <div className="col-5 p-4">
         <div style={containerStyle}>
           <h1>Soundpeat</h1>
+          <RoundIndicator currentRound={2} />
+          <PlayerIndicator
+            totalPlayers={totalPlayers}
+            currentPlayer={currentPlayer}
+          />
+          <div className="row mt-4">
+            <div className="col">
+              <Button color="danger">End game</Button>
+            </div>
+          </div>
         </div>
+        <NewGameModal />
       </div>
     );
   }
